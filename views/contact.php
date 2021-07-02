5bd1fc55c90c8d380bf114d5636fbfef2840426f
Mumbi Francis Kinyanjui <kinya.francis.mumbi@gmail.com>
2021-07-02 09:15:07 +0300

    Added some 404 && Contact view

---
+++ views/contact.php
@@ -0,0 +1,7 @@
+<form action="/contact" method="post">
+    <label>First name:</label><br>
+    <input type="text" name="fname" value="John"><br>
+    <label>Last name:</label><br>
+    <input type="text" name="lname" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+</form>
